ea68163938fa485d86c46304b00cd904e839dfb7
Tiago Alves <alvesjtiago@gmail.com>
2024-04-18 19:05:02 +0100

    Initial commit

---
+++ app/api/cron-crawler/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import { check } from 'linkinator'
+
+export async function GET(request: Request) {
+  const results = await check({
+    path: process.env.URLS?.split(',').map((link) => link.trim()) as string[],
+  })
+
+  const brokenLinks = results.links.filter(
+    (x) => x.state === 'BROKEN' && x.status == 404,
+  )
+  console.log(`Detected ${brokenLinks.length} broken links.`)
+  console.log(`Broken links: ${brokenLinks.map((link) => link.url).join(', ')}`)
+
+  if (!results.passed) {
+    console.log('Send to Zapier')
+  }
+
+  return NextResponse.json({
+    success: true,
+    message: `Passed: ${brokenLinks.length == 0}`,
+  })
+}
