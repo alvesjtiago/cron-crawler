34895640eb0ee8c8ed2b1e96ad9cc4f4c9533750
Tiago Alves <alvesjtiago@gmail.com>
2024-04-18 20:17:27 +0100

    Add recurse flag

--- app/api/cron-crawler/route.ts
+++ app/api/cron-crawler/route.ts
@@ -4,6 +4,7 @@ import { check } from 'linkinator'
 export async function GET(request: Request) {
   const results = await check({
     path: process.env.URLS?.split(',').map((link) => link.trim()) as string[],
+    recurse: true,
   })
 
   const brokenLinks = results.links.filter(
